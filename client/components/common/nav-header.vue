808d1a3aedc8f3cef3d531c7cb38cb1998bb2e73
timzaak <zsy.evan@gmail.com>
2025-12-22 17:11:53 +0800

    remove alpha

--- client/components/common/nav-header.vue
+++ client/components/common/nav-header.vue
@@ -44,8 +44,7 @@
           //-         v-list-item-title.body-2.grey--text.text--ligten-2 {{$t('common:header.imagesFiles')}}
           //-         v-list-item-subtitle.overline.grey--text.text--lighten-2 Coming soon
           v-toolbar-title(:class='{ "mx-3": $vuetify.breakpoint.mdAndUp, "mx-1": $vuetify.breakpoint.smAndDown }')
-            span.subheading {{title + `&nbsp;`}}
-              sup alpha
+            span.subheading {{title}}
       v-flex(md4, v-if='$vuetify.breakpoint.mdAndUp')
         v-toolbar.nav-header-inner(color='black', dark, flat)
           slot(name='mid')
